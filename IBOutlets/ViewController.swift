a3cbb42fabb49cfe8bb6a9eb8a96067bee48e66b
Luz Itzel Valdeolivar <li.valdeolivar@mail.com>
2024-09-06 14:28:33 -0600

    Initial Commit

---
+++ IBOutlets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBOutlets
+//
+//  Created by Viankail Cedillo Castelan on 9/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
