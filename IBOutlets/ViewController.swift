f85542a3024923932079c9d2978cc145cc03199c
Luz Itzel Valdeolivar <li.valdeolivar@mail.com>
2024-09-06 16:25:56 -0600

    final version

--- IBOutlets/ViewController.swift
+++ IBOutlets/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pokemonimage: UIImageView!
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+        var pokemon = Int.random(in: 0..<23)
+        pokemonimage.image = UIImage(named: String(describing: pokemon))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
